895672ed9a3dcade3c3df13c30919cd868af90e5
DGMWdev <dylan.wanten.dev@gmail.com>
2021-04-29 00:12:27 +0200

    add user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_185826) do
+ActiveRecord::Schema.define(version: 2021_04_28_220803) do
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_04_27_185826) do
     t.date "date"
     t.integer "duration"
     t.integer "repetitions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
